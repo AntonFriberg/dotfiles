5062d4fdd1c57cd6dec1b5d1778a8ab44771ef41
Anton Friberg <anton.friberg@outlook.com>
2018-08-20 17:25:01 +0200

    Add better support for second monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,9 +5,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1)
-# export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
+export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
 
 polybar bar &
-# polybar second &
+polybar second &
 
 echo "Bars launched..."
