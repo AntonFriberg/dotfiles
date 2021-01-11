9cb5fb9e3d03077bb3a4634d8585215d2fc5c348
Anton Friberg <anton.friberg@outlook.com>
2021-01-11 16:23:57 +0100

    Change p10k prompt - green user@host instead

--- .p10k.zsh
+++ .p10k.zsh
@@ -40,6 +40,7 @@
   # Prompt colors.
   local grey='242'
   local red='1'
+  local green='2'
   local yellow='3'
   local blue='4'
   local magenta='5'
@@ -104,9 +105,9 @@
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=$blue
 
   # Context format when root: user@host. The first part white, the rest grey.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$white}%n%f%F{$grey}@%m%f"
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$white}%n%f%F{$green}@%m%f"
   # Context format when not root: user@host. The whole thing grey.
-  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$grey}%n@%m%f"
+  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$green}%n@%m%f"
   # Don't show context unless root or in SSH.
   # typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION=
 
