a674390416808888c448a3b274ce89e14411a014
Anton Friberg <anton.friberg@outlook.com>
2021-03-21 10:49:58 +0100

    Move prompt initialization to plugins-conf.zsh

--- .config/zsh/lib/plugins-conf.zsh
+++ .config/zsh/lib/plugins-conf.zsh
@@ -15,3 +15,10 @@ HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
 ### alias-tips
 export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias tip: "
 #export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ ll vi"
+
+################
+# SETUP PROMPT #
+################
+
+# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
