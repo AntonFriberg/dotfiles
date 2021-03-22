d1211880feb00f7e42fa50e2eccae78df6466bc0
Anton Friberg <anton.friberg@outlook.com>
2021-03-22 14:12:32 +0100

    Use ZDOTDIR environment in plugins-conf.zsh

--- .config/zsh/lib/plugins-conf.zsh
+++ .config/zsh/lib/plugins-conf.zsh
@@ -21,4 +21,4 @@ export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias tip: "
 ################
 
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+[[ ! -f ${ZDOTDIR:-$HOME}/.p10k.zsh ]] || source ${ZDOTDIR:-$HOME}/.p10k.zsh
