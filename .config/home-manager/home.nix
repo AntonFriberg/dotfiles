e4ae0762d633c85392997ab711991a25e9e70c8e
Anton Friberg <anton.friberg@outlook.com>
2023-06-28 08:42:37 +0200

    Add packages to home-manager installation

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -9,8 +9,47 @@
     stateVersion = "22.11";
 
     packages = with pkgs; [
+      alejandra
+      delta
+      docker-compose
+      fuzzel
+      fzf
       gh
       git
+      git-crypt
+      glances
+      go-migrate
+      helmfile
+      httpie
+      ipcalc
+      jq
+      k9s
+      kubectl
+      kubectl-node-shell
+      kubeconform
+      kubeseal
+      kubectx
+      kubernetes-helm
+      ncdu
+      openssh
+      radeontop
+      rclone
+      ripgrep
+      tealdeer
+      vagrant
+      yt-dlp
+      (nerdfonts.override {
+        fonts = [
+          "Hack"
+          "FiraCode"
+          "FiraMono"
+          "CascadiaCode"
+          "Cousine"
+          "DroidSansMono"
+          "JetBrainsMono"
+          "SourceCodePro"
+        ];
+      })
     ];
   };
 
