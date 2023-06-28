81dd2d82443f926fb98c48c3bb9a2b478c352dbf
Anton Friberg <anton.friberg@outlook.com>
2023-06-28 09:54:40 +0200

    Add fish plugins to home-manager config

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -99,6 +99,37 @@
   # Change shell to fish
   programs.fish = {
     enable = true;
+    plugins = [
+      {
+        # After first install you need to call the setup script manually
+        # fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
+        name = "tide";
+        src = pkgs.fetchFromGitHub {
+          owner = "IlanCosman";
+          repo = "tide";
+          rev = "6833806ba2eaa1a2d72a5015f59c284f06c1d2db";
+          sha256 = "1qqkxpi4pl0s507gj4xv7b58ykbqzxbhxmw949ja3srph9i2qbmy";
+        };
+      }
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "b3dd471bcc885b597c3922e4de836e06415e52dd";
+          sha256 = "13wdsvpazivlxk921ccqbk7gl6ya2md8f45rckbn8rn119ckf7fy";
+        };
+      }
+      {
+        name = "bang-bang";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-bang-bang";
+          rev = "816c66df34e1cb94a476fa6418d46206ef84e8d3";
+          sha256 = "0dx4z0mmrwfkg8qh1yis75vwf69ng51m3icsiiw7k2cwc02mg76z";
+        };
+      }
+    ];
   };
 
   # SSH Agent systemd user service
