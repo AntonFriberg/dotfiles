b0bbfea4fab051cd93de57dcb3d56f9efaa7deab
Anton Friberg <anton.friberg@outlook.com>
2023-06-28 09:55:39 +0200

    Add envsource function to fish config

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -130,6 +130,16 @@
         };
       }
     ];
+    functions = {
+      # Custom function for sourcing .env files
+      envsource = ''
+        for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
+          set item (string split -m 1 '=' $line)
+          set -gx $item[1] (string trim --chars=\'\" $item[2])
+          echo "Exported key $item[1]"
+        end
+      '';
+    };
   };
 
   # SSH Agent systemd user service
