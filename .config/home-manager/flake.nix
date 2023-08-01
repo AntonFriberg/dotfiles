d041c4281e228da5203275ad97f61864100dccb1
Anton Friberg <anton.friberg@outlook.com>
2023-08-01 11:12:31 +0200

    Change to multi-host home-manager setup
    In some cases we want different home-manager configs depending on which
    host/user combination we are targeting. This new setup allows this.
    
    Since the hostname in some cases might be sensitive we utilize a YADM
    encrypted nix file to store the strings.

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -29,18 +29,24 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    # Secrets from YADM encrypted file
+    secrets = import ./secrets.nix;
+    users = secrets.users;
+    hosts = secrets.hosts;
   in {
     formatter.${system} = pkgs.alejandra;
 
-    homeConfigurations.antonfr = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+    homeConfigurations = {
+      "${users.work}@${hosts.work.laptop}" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [./home.nix];
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [./home.nix];
 
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
   };
 }
