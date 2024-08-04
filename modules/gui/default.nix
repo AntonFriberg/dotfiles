2d15db87602a8c75c3f45ac3d1d8b31d6cc76a64
Anton Friberg <anton.friberg@outlook.com>
2024-08-04 22:13:45 +0200

    Add vscode to Flake setup

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ./alacritty.nix
+    ./vscode.nix
     # Fix for GPU stuff on non-nixos systems
     ./nixGL.nix
   ];
---
+++ modules/gui/vscode.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.vscode = {
+    enable = true;
+    package = config.lib.nixGL.wrap pkgs.vscode;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = true;
+    extensions = with pkgs.vscode-extensions; [
+      arcticicestudio.nord-visual-studio-code
+      bbenoist.nix
+      bmalehorn.vscode-fish
+      esbenp.prettier-vscode
+      kamadorueda.alejandra
+      mhutchie.git-graph
+      ms-python.python
+      ms-python.vscode-pylance
+      ms-vscode-remote.remote-ssh
+      redhat.vscode-yaml
+      tamasfe.even-better-toml
+    ];
+    userSettings = {
+      "[json]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+      "[python]" = {"editor.defaultFormatter" = null;};
+      "[yaml]" = {"editor.defaultFormatter" = "redhat.vscode-yaml";};
+      "editor.copyWithSyntaxHighlighting" = false;
+      "editor.fontFamily" = "'Cousine', 'Hack', 'monospace', monospace, 'Droid Sans Fallback'";
+      "editor.minimap.enabled" = false;
+      "editor.renderWhitespace" = "boundary";
+      "editor.rulers" = [80 120];
+      "files.insertFinalNewline" = true;
+      "files.trimTrailingWhitespace" = true;
+      "python.analysis.typeCheckingMode" = "basic";
+      "python.languageServer" = "Pylance";
+      "terminal.integrated.fontFamily" = "Cousine Nerd Font Mono";
+      "window.titleBarStyle" = "custom";
+      "window.zoomLevel" = 1;
+      "workbench.colorTheme" = "Nord";
+      "workbench.sideBar.location" = "right";
+      "extensions.ignoreRecommendations" = true;
+    };
+  };
+}
