c415c81eb4fb5c244976641139c74783adfe75b0
Anton Friberg <anton.friberg@outlook.com>
2024-08-05 18:57:09 +0200

    Set session variables

--- modules/home.nix
+++ modules/home.nix
@@ -3,6 +3,29 @@
     username = "antonfr";
     homeDirectory = "/home/antonfr";
     stateVersion = "23.11";
+    sessionVariables = {
+      # Set default applications
+      SHELL = "fish";
+      TERMINAL = "alacritty";
+      VISUAL = "vim";
+      EDITOR = "vim";
+      BROWSER = "firefox";
+      PAGER = "less";
+      # Set XDG directories
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_BIN_HOME = "$HOME/.local/bin";
+      XDG_LIB_HOME = "$HOME/.local/lib";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      # Respect XDG directories
+      DOCKER_CONFIG = "$HOME/.config/docker";
+      LESSHISTFILE = "-"; # Disable less history
+      # Python config
+      PYTHONDONTWRITEBYTECODE = "true";
+      PIP_REQUIRE_VIRTUALENV = "true";
+      # SSH Agent
+      SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent";
+    };
   };
 
   programs.home-manager.enable = true;
