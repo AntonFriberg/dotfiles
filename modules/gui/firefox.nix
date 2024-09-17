13b32f8dc34ecaef61a0ea0737dbf7606a586935
Anton Friberg <anton.friberg@outlook.com>
2024-09-17 16:14:59 +0200

    Firefox - configure custom search engines

--- modules/gui/firefox.nix
+++ modules/gui/firefox.nix
@@ -42,6 +42,125 @@
           "dom.battery.enabled" = false; # you don't need to see my battery...
           "dom.private-attribution.submission.enabled" = false; # https://news.ycombinator.com/item?id=40974112
         };
+        search = {
+          force = true;
+          default = "Google";
+          order = ["Google" "Youtube" "GitHub" "HackerNews" "Kagi" "Nix Packages" "Home Manager" "NixOS Options"];
+          engines = {
+            "Bing".metaData.hidden = true;
+            "Amazon.com".metaData.hidden = true;
+            "Wikipedia (en)".metaData.hidden = true;
+            "DuckDuckGo".metaData.hidden = true;
+
+            "Goalie" = {
+              iconUpdateURL = "https://go.se.axis.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              urls = [{template = "https://go.se.axis.com/{searchTerms}";}];
+              definedAliases = ["@go"];
+            };
+            "YouTube" = {
+              iconUpdateURL = "https://youtube.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@yt"];
+              urls = [
+                {
+                  template = "https://www.youtube.com/results";
+                  params = [
+                    {
+                      name = "search_query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "Nix Packages" = {
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "NixOS Options" = {
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@no"];
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "GitHub" = {
+              iconUpdateURL = "https://github.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@gh"];
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "Home Manager" = {
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@hm"];
+              urls = [
+                {
+                  template = "https://mipmip.github.io/home-manager-option-search/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "HackerNews" = {
+              iconUpdateURL = "https://hn.algolia.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@hn"];
+              urls = [
+                {
+                  template = "https://hn.algolia.com/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+          };
+        };
       };
     };
   };
