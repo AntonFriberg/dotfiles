cc8ad5ef3f92721193699c3a78a4691378c86907
Anton Friberg <anton.friberg@outlook.com>
2024-09-17 16:10:35 +0200

    Firefox - disable stuff I find useless

--- modules/gui/firefox.nix
+++ modules/gui/firefox.nix
@@ -24,6 +24,18 @@
           "browser.aboutConfig.showWarning" = false; # I sometimes know what I'm doing
           "browser.translations.neverTranslateLanguages" = "sv"; # No need to translate swedish
           "privacy.clearOnShutdown.history" = false; # We want to save history on exit
+          # Disable some useless stuff
+          "extensions.pocket.enabled" = false; # disable pocket, save links, send tabs
+          "extensions.abuseReport.enabled" = false; # don't show 'report abuse' in extensions
+          "extensions.formautofill.creditCards.enabled" = false; # don't auto-fill credit card information
+          "identity.fxaccounts.enabled" = false; # disable firefox login
+          "identity.fxaccounts.toolbar.enabled" = false;
+          "identity.fxaccounts.pairing.enabled" = false;
+          "identity.fxaccounts.commands.enabled" = false;
+          "browser.contentblocking.report.lockwise.enabled" = false; # don't use firefox password manger
+          "browser.uitour.enabled" = false; # no tutorial please
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
         };
       };
     };
