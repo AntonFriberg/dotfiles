c33179cf5ca80c3af811328cdc674d144f398911
Anton Friberg <anton.friberg@outlook.com>
2024-09-26 10:20:32 +0200

    Add clickhouse to work module

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -3,6 +3,13 @@
   pkgs,
   ...
 }: {
+  # Add packages
+  home.packages = lib.mkMerge [
+    (with pkgs; [
+      clickhouse
+    ])
+  ];
+
   # Take system packages for git and ssh since I could not get Nix to support GSSAPIKeyExchange that is enabled at work.
   # https://github.com/nix-community/home-manager/issues/4763#issuecomment-1986996921
   programs.git.package = lib.mkForce pkgs.emptyDirectory;
