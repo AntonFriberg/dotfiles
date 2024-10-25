f4fe0fb9e47c4afffa112fce2d6bb85cac8f6f7a
Anton Frost <anton.friberg@outlook.com>
2024-10-25 13:15:50 +0200

    Move to official nixGL implementation since merged to HM
    The nixGL pull request on home manager repo has now been merged. This
    means that we can remove the dependency on importing functionality from
    PR.

--- modules/gui/nixgl.nix
+++ modules/gui/nixgl.nix
@@ -3,14 +3,6 @@
   pkgs,
   ...
 }: {
-  imports = [
-    # TODO: remove when https://github.com/nix-community/home-manager/pull/5355 gets merged:
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "1krclaga358k3swz2n5wbni1b2r7mcxdzr6d7im6b66w3sbpvnb3";
-    })
-  ];
-
   nixGL = {
     packages = nixGL.packages; # you must set this or everything will be a noop
     defaultWrapper = "mesa"; # choose from options
