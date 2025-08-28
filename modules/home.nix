c7f8a5481293750c27d554b68bf9bd132962c5f9
Anton Frost <anton.friberg@outlook.com>
2025-08-28 15:36:47 +0200

    Fix broken firefox emoji font rendering
    The old fixes for ugly fonts in debian seems to break firefox emoji
    rendering. I do not think this customization is necessary anymore since
    modern Linux distros seem to be better configured out of the box
    nowadays.

--- modules/home.nix
+++ modules/home.nix
@@ -42,36 +42,4 @@
   xdg.configFile."environment.d/envvars.conf".text = ''
     PATH="$HOME/.nix-profile/bin:$PATH"
   '';
-
-  # Better font rendering
-  xdg.configFile."fontconfig/conf.d/1-better-rendering.conf".text = ''
-    <!-- Better font rendering by changing settings. -->
-    <!-- https://wiki.archlinux.org/title/font_configuration -->
-    <match target="font">
-      <!-- disable embedded bitmaps in fonts to fix Calibri, Cambria, etc. -->
-      <edit name="embeddedbitmap" mode="assign">
-        <bool>false</bool>
-      </edit>
-      <!-- Enable anti-aliasing in font rendering -->
-      <edit mode="assign" name="antialias">
-        <bool>true</bool>
-      </edit>
-      <!-- Enable hinting in font rendering -->
-      <edit mode="assign" name="hinting">
-        <bool>true</bool>
-      </edit>
-      <!-- Only hint slightly -->
-      <edit mode="assign" name="hintstyle">
-        <const>hintslight</const>
-      </edit>
-      <!-- Set subpixel rendering to reduce color fringing -->
-      <edit mode="assign" name="lcdfilter">
-        <const>lcddefault</const>
-      </edit>
-      <!-- Set correct pixel alignment-->
-      <edit mode="assign" name="rgba">
-        <const>rgb</const>
-      </edit>
-    </match>
-  '';
 }
