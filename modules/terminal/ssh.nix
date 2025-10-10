bc7af110f08ab0af498d4c9376bf21fe6e4444f8
Anton Frost <anton.friberg@outlook.com>
2025-10-10 11:13:08 +0200

    Fix ssh warnings

--- modules/terminal/ssh.nix
+++ modules/terminal/ssh.nix
@@ -1,18 +1,26 @@
 {...}: {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
-    compression = true;
-    controlMaster = "auto";
-    controlPersist = "10m";
-    serverAliveInterval = 25;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      compression = true;
+      controlMaster = "auto";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "10m";
+      forwardAgent = false;
+      forwardX11 = false;
+      hashKnownHosts = false;
+      serverAliveCountMax = 3;
+      serverAliveInterval = 25;
+      setEnv = {
+        TERM = "xterm-256color";
+      };
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
     includes = [
       "config.d/*"
     ];
-    extraConfig = ''
-      SetEnv TERM=xterm-256color
-      ForwardX11 no
-    '';
   };
 
   # SSH Agent systemd user service
