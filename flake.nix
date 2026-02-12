3d1b1fdd260c8d6dbaf348e994d6eddcb35c45e6
Anton Frost <anton.friberg@outlook.com>
2026-02-12 17:34:43 +0100

    Update home-manager deps and change to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
