061a32114f7e6241d2e37401c3b48e759ee30e05
Anton Frost <anton.friberg@outlook.com>
2026-05-11 12:47:59 +0200

    Move firefox config path to new default

--- modules/gui/firefox.nix
+++ modules/gui/firefox.nix
@@ -7,6 +7,7 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-devedition;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles = {
       dev-edition-default = {
         id = 0;
